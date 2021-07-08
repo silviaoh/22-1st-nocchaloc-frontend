5f85349abc206441418501db29ee302a4063b8ed
silviaoh <silvia.jisu.oh@gmail.com>
2021-07-08 15:45:13 +0900

    Modify: Link -> button 태그로 수정

--- src/pages/Main/Tealist/Tea/Tea.jsx
+++ src/pages/Main/Tealist/Tea/Tea.jsx
@@ -12,9 +12,9 @@ class Tea extends Component {
         <Link className="tea-img-container">
           <img src={main_img_url} alt="Tea" className="tea-img-before" />
           <img src={hover_img_url} alt="Tea" className="tea-img-after" />
-          <Link className="cart">
+          <button className="cart">
             <i class="fas fa-shopping-cart"></i>
-          </Link>
+          </button>
         </Link>
         <p className="name">
           <Link to="" className="name-link">
