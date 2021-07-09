46ab4481d32052b2a7842a18f7b9b82308dc1370
Soojong Kim <soojongkim@Soojongui-MacBookPro.local>
2021-07-09 15:26:22 +0900

    메인페이지 레이아웃 리팩토링

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import NavData from './NavData';
-// import NavList from './NavData';
-import '../../styles/common.scss';
 import './Nav.scss';
 
 class Nav extends React.Component {
   render() {
+    // const NAV_LIST = [
+    //   {name:"티샵", path="/tea-life"},
+    //   {name:"이벤트", path="/event"},
+    // ]
     return (
       <>
         {/* Navigation menu */}
-        <div className="nav-container">
+        <div className="Nav">
           <div className="nav-inner">
-            <img src="/images/Main/logo.svg" className="logo" alt="logo"></img>
+            <img alt="logo" src="/images/Main/logo.svg" className="logo" />
             <Link to="/"></Link>
             <nav className="navbar">
               <div className="nav-menu">
                 <ul className="menu-list">
                   <li>
                     <Link to="/">티샵</Link>
-                    <ul>
-                      {/* <li>티</li>
-                      <li>티2</li>
-                      <li>티3</li> */}
-                    </ul>
                   </li>
                   <li>
                     <Link to="/">티라이프</Link>
@@ -40,39 +36,6 @@ class Nav extends React.Component {
                 </ul>
               </div>
             </nav>
-            {/* inner */}
-            {/* <div id="w_lnb" className="w_lnb active">
-              <div className="nav-inner">
-                <div className="menu_wrap"></div>
-                <div className="menu-narrow">
-                  <h3 className="nav-hidden">티샵</h3>
-                  <ul>
-                    <li className="category">
-                      <Link to="/">티/티푸드</Link>
-                      <div className="depth">
-                        <Link to="/">전체보기</Link>
-                        <div className="sub_depth">
-                          <div className="sub_box">
-                            <Link to="/">티</Link>
-                            <ul className="sub_menu">
-                              <li>명차</li>
-                              <li>녹차/발효차/홍차</li>
-                              <li>허브티(무카페인)</li>
-                              <li>블렌디드티</li>
-                              <li>웰니스티</li>
-                              <li>파우더</li>
-                              <li>세트</li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div> */}
-
-            {/* inner 끝 */}
             <div className="nav-side">
               <div className="menubox-icon">
                 <i class="fas fa-search"></i>
