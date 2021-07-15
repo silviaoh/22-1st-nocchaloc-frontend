788207bbd040596f0369550f06fd03ac9721a5f3
silviaoh <silvia.jisu.oh@gmail.com>
2021-07-15 09:56:06 +0900

    Add: carousel 차 카테고리 이름 누르면 동영상 부드럽게 이동

--- src/pages/Main/Tealist/VideoSlide/VideoSlide.jsx
+++ src/pages/Main/Tealist/VideoSlide/VideoSlide.jsx
@@ -4,14 +4,31 @@ import Slide from './Slide/Slide';
 import './VideoSlide.scss';
 
 class VideoSlide extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      videoButtonId: 0,
+      videoCount: 0,
+    };
+    this.swiperInner = React.createRef();
+  }
+
+  clickTeaName = id => {
+    id !== 0 && this.setState({ isFirst: false });
+    this.setState({ videoButtonId: id });
+    this.swiperInner.current.style.transform = `translate3d(${
+      25 - 50 * (id - 1)
+    }vw, 0, 0)`;
+  };
+
   render() {
-    const { products, video } = this.props;
+    const { products, video, CATEGORY } = this.props;
 
     return (
       <div className="tea-carousel">
         <div className="swiper-container">
           <div className="swiper">
-            <ul className="swiper-inner" ref={this.innerul}>
+            <ul className="swiper-inner" ref={this.swiperInner}>
               {video.map(video => (
                 <Slide key={video.id} video={video} />
               ))}
@@ -21,7 +38,7 @@ class VideoSlide extends React.Component {
                 <i className="fas fa-chevron-left" />
               </button>
             </div>
-            <div className="transparentbox right-0" ref={this.rightBox}>
+            <div className="transparentbox right-0">
               <button className="right">
                 <i className="fas fa-chevron-right" />
               </button>
@@ -30,12 +47,18 @@ class VideoSlide extends React.Component {
         </div>
         <div className="swiper-teaname">
           <div className="teaname-overflow">
-            {products.category_info &&
-              products.category_info.map(menu => (
-                <Link className="teaname" key={menu.id}>
-                  {menu.name}
-                </Link>
-              ))}
+            {CATEGORY.map((category, idx) => (
+              <button
+                className={`teaname ${
+                  (this.state.videoButtonId === category.id && 'active') ||
+                  (this.state.videoButtonId === 0 && idx === 0 && 'active')
+                }`}
+                key={category.id}
+                onClick={() => this.clickTeaName(category.id)}
+              >
+                {category.name}
+              </button>
+            ))}
           </div>
         </div>
       </div>
